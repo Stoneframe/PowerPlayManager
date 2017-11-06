1c84aaea5ffa8344a3dc8832afa36bd7477a2313
Paul Steneram Bibby <paul.steneram.bibby@gmail.com>
2017-11-06 21:21:42 +0100

    Reorder methods in PlayerEvaluator

--- src/evaluators/PlayerEvaluator.java
+++ src/evaluators/PlayerEvaluator.java
@@ -34,6 +34,91 @@ public class PlayerEvaluator<A extends Attributes>
 		this.facilityEvaluator = facilityEvaluator;
 	}
 
+	public void setAttributeEvaluators(List<AttributeEvaluator<A>> attributeEvaluators)
+	{
+		this.attributeEvaluators = attributeEvaluators;
+	}
+
+	public int getFacilityLevel()
+	{
+		return facilityLevel;
+	}
+
+	public void setFacilityLevel(int facilityLevel)
+	{
+		this.facilityLevel = facilityLevel;
+	}
+
+	public int getStaffEffectivness()
+	{
+		return staffEffectivness;
+	}
+
+	public void setStaffEffectivness(int staffEffectivness)
+	{
+		this.staffEffectivness = staffEffectivness;
+	}
+
+	public PositionNameValue getBestPositionRating(Player<A> player)
+	{
+		AttributeEvaluator<A> evaluator = attributeEvaluators
+				.stream()
+				.max(new RatingEvaluatorComparator<A>(player.getAttributes())::compare)
+				.get();
+
+		return new PositionNameValue(
+				evaluator.getName(),
+				evaluator.getRating(player.getAttributes()));
+	}
+
+	public PositionNameValue getBestPositionQuality(Player<A> player)
+	{
+		AttributeEvaluator<A> evaluator = attributeEvaluators
+				.stream()
+				.max(new QualityEvaluatorComparator<A>(player.getAttributes())::compare)
+				.get();
+
+		return new PositionNameValue(
+				evaluator.getName(),
+				evaluator.getQuality(player.getAttributes()));
+	}
+
+	public double calculateRatingForAge(Player<A> player, int age)
+	{
+		return F(player.getAge(), age) * DAYS_PER_SEASON * getTrainingValue(player)
+				+ player.getAttributes().getTotalRating();
+	}
+
+	private double getTrainingValue(Player<A> player)
+	{
+		double training = player.getTraining() != 0
+				? player.getTraining()
+				: calculatePlayerTraining(player);
+
+		return training / f(player.getAge());
+	}
+
+	private double calculatePlayerTraining(Player<A> player)
+	{
+		return getTrainingFacilityEffectivness() * getEstimatedPlayerTraining(player);
+	}
+
+	private double getTrainingFacilityEffectivness()
+	{
+		double facilityEffectivness = facilityEvaluator
+				.getOverallEffectivness(facilityLevel, staffEffectivness);
+
+		return (40 + facilityEffectivness) / 145;
+	}
+
+	private double getEstimatedPlayerTraining(Player<A> player)
+	{
+		return -0.02278 * player.getAge()
+				+ 0.1291 * player.getCL()
+				- 0.0003325 * player.getAttributes().getAverageQuality()
+				+ 0.009671 * getBestPositionQuality(player).getValue();
+	}
+
 	private double F(int a, int b)
 	{
 		boolean invert = false;
@@ -62,93 +147,8 @@ public class PlayerEvaluator<A extends Attributes>
 		return invert ? -sum : sum;
 	}
 
-	private double calculatePlayerTraining(Player<A> player)
-	{
-		return getTrainingFacilityEffectivness() * getEstimatedPlayerTraining(player);
-	}
-
-	public double calculateRatingForAge(Player<A> player, int age)
-	{
-		return F(player.getAge(), age) * DAYS_PER_SEASON * getTrainingValue(player)
-				+ player.getAttributes().getTotalRating();
-	}
-
 	private double f(int x)
 	{
 		return a * Math.pow(x, 2) + b * Math.pow(x, 1) + c;
-	}
-
-	public void setAttributeEvaluators(List<AttributeEvaluator<A>> attributeEvaluators)
-	{
-		this.attributeEvaluators = attributeEvaluators;
-	}
-
-	public PositionNameValue getBestPositionRating(Player<A> player)
-	{
-		AttributeEvaluator<A> evaluator = attributeEvaluators
-				.stream()
-				.max(new RatingEvaluatorComparator<A>(player.getAttributes())::compare)
-				.get();
-
-		return new PositionNameValue(
-				evaluator.getName(),
-				evaluator.getRating(player.getAttributes()));
-	}
-
-	public PositionNameValue getBestPositionQuality(Player<A> player)
-	{
-		AttributeEvaluator<A> evaluator = attributeEvaluators
-				.stream()
-				.max(new QualityEvaluatorComparator<A>(player.getAttributes())::compare)
-				.get();
-
-		return new PositionNameValue(
-				evaluator.getName(),
-				evaluator.getQuality(player.getAttributes()));
-	}
-
-	private double getEstimatedPlayerTraining(Player<A> player)
-	{
-		return -0.02278 * player.getAge()
-				+ 0.1291 * player.getCL()
-				- 0.0003325 * player.getAttributes().getAverageQuality()
-				+ 0.009671 * getBestPositionQuality(player).getValue();
-	}
-
-	public int getFacilityLevel()
-	{
-		return facilityLevel;
-	}
-
-	public void setFacilityLevel(int facilityLevel)
-	{
-		this.facilityLevel = facilityLevel;
-	}
-
-	public int getStaffEffectivness()
-	{
-		return staffEffectivness;
-	}
-
-	public void setStaffEffectivness(int staffEffectivness)
-	{
-		this.staffEffectivness = staffEffectivness;
-	}
-
-	private double getTrainingFacilityEffectivness()
-	{
-		double facilityEffectivness = facilityEvaluator
-				.getOverallEffectivness(facilityLevel, staffEffectivness);
-
-		return (40 + facilityEffectivness) / 145;
-	}
-
-	private double getTrainingValue(Player<A> player)
-	{
-		double training = player.getTraining() != 0
-				? player.getTraining()
-				: calculatePlayerTraining(player);
-
-		return training / f(player.getAge());
 	}
 }
