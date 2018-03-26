78410b1bae88e6651851282be2933838413adb8d
Paul Steneram Bibby <paul.steneram.bibby@gmail.com>
2018-03-26 19:24:29 +0200

    Add functionallity to move groups up and down

--- src/gui/GroupPanel.java
+++ src/gui/GroupPanel.java
@@ -3,6 +3,7 @@ package gui;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -11,12 +12,14 @@ import java.util.Collections;
 import java.util.List;
 
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
 import model.Attributes;
@@ -34,6 +37,9 @@ public class GroupPanel<A extends Attributes>
 	private JButton addButton;
 	private JButton removeButton;
 
+	private JButton moveUpButton;
+	private JButton moveDownButton;
+
 	public GroupPanel()
 	{
 		groupListModel = new DefaultListModel<>();
@@ -42,6 +48,19 @@ public class GroupPanel<A extends Attributes>
 		groupList.setPreferredSize(new Dimension(250, 350));
 		groupList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
 		groupList.setBorder(BorderFactory.createEtchedBorder());
+		groupList.addListSelectionListener(new ListSelectionListener()
+		{
+			public void valueChanged(ListSelectionEvent e)
+			{
+				moveUpButton.setEnabled(
+					groupList.getSelectedIndices().length == 1
+							&& groupList.getSelectedIndex() != 0);
+
+				moveDownButton.setEnabled(
+					groupList.getSelectedIndices().length == 1
+							&& groupList.getSelectedIndex() != groupListModel.size() - 1);
+			}
+		});
 		groupList.addMouseListener(new MouseAdapter()
 		{
 			public void mouseClicked(MouseEvent evt)
@@ -56,7 +75,7 @@ public class GroupPanel<A extends Attributes>
 					if (newName != null)
 					{
 						group.setName(newName);
-						
+
 						groupList.updateUI();
 					}
 				}
@@ -69,18 +88,53 @@ public class GroupPanel<A extends Attributes>
 		removeButton = new JButton("Remove");
 		removeButton.setEnabled(false);
 
+		moveUpButton = new JButton("▲");
+		moveUpButton.setEnabled(false);
+		moveUpButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				int index = groupList.getSelectedIndex();
+
+				Roster<A>.Group group = groupListModel.remove(index);
+				groupListModel.add(index - 1, group);
+
+				addGroupToSelectedGroups(group);
+			}
+		});
+
+		moveDownButton = new JButton("▼");
+		moveDownButton.setEnabled(false);
+		moveDownButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				int index = groupList.getSelectedIndex();
+
+				Roster<A>.Group group = groupListModel.remove(index);
+				groupListModel.add(index + 1, group);
+
+				addGroupToSelectedGroups(group);
+			}
+		});
+
 		setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
 		setLayout(new BorderLayout());
 
+		JPanel buttonsUpDownPanel = new JPanel();
+		buttonsUpDownPanel.setLayout(new BoxLayout(buttonsUpDownPanel, BoxLayout.Y_AXIS));
+		buttonsUpDownPanel.add(moveUpButton);
+		buttonsUpDownPanel.add(moveDownButton);
+
+		JPanel buttonAddRemovePanel = new JPanel();
+		buttonAddRemovePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		buttonAddRemovePanel.add(addButton);
+		buttonAddRemovePanel.add(removeButton);
+
 		add(groupList, BorderLayout.CENTER);
-
-		JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-
-		buttonPanel.add(addButton);
-		buttonPanel.add(removeButton);
-
-		add(buttonPanel, BorderLayout.SOUTH);
+		add(buttonsUpDownPanel, BorderLayout.EAST);
+		add(buttonAddRemovePanel, BorderLayout.SOUTH);
 	}
 
 	public void addGroup(Roster<A>.Group group)
@@ -89,13 +143,7 @@ public class GroupPanel<A extends Attributes>
 
 		if (group.isEnabled())
 		{
-			final int N = groupList.getSelectedIndices().length;
-
-			int[] indices = Arrays.copyOf(groupList.getSelectedIndices(), N + 1);
-
-			indices[N] = groupListModel.indexOf(group);
-
-			groupList.setSelectedIndices(indices);
+			addGroupToSelectedGroups(group);
 		}
 	}
 
@@ -137,5 +185,16 @@ public class GroupPanel<A extends Attributes>
 	public void setRemoveButtonEnabled(boolean isEnabled)
 	{
 		removeButton.setEnabled(isEnabled);
+	}
+
+	private void addGroupToSelectedGroups(Roster<A>.Group group)
+	{
+		final int N = groupList.getSelectedIndices().length;
+
+		int[] indices = Arrays.copyOf(groupList.getSelectedIndices(), N + 1);
+
+		indices[N] = groupListModel.indexOf(group);
+
+		groupList.setSelectedIndices(indices);
 	}
 }
