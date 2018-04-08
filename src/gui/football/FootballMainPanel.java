b50e7c0f6cbbe66cabbf8036e4ed770c82b259c3
Paul Steneram Bibby <paul.steneram.bibby@gmail.com>
2018-04-08 12:12:08 +0200

    Add football main panel

---
+++ src/gui/football/FootballMainPanel.java
@@ -0,0 +1,27 @@
+package gui.football;
+
+import java.util.List;
+
+import evaluators.PlayerEvaluator;
+import formation.PaulsFormationBuilder;
+import gui.MainPanel;
+import model.football.FootballAttributes;
+import parsers.players.PlayersParser;
+
+public class FootballMainPanel extends MainPanel<FootballAttributes>
+{
+	private static final long serialVersionUID = -4461292547199081549L;
+
+	public FootballMainPanel(
+			List<PlayersParser<FootballAttributes>> parsers,
+			PlayerEvaluator<FootballAttributes> playerEvaluator)
+	{
+		super(
+			new FootballAttributesPanel(),
+			new FootballTrainingPanel(playerEvaluator),
+			new FootballFormationTemplatePanelFactory(),
+			new PaulsFormationBuilder<>(),
+			parsers,
+			playerEvaluator);
+	}
+}
