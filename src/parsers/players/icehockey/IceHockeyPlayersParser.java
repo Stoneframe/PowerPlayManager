e12282c75658036ac653e32a570fbf8d5d178024
Paul Steneram Bibby <paul.steneram.bibby@gmail.com>
2018-07-01 13:38:36 +0200

    Convert IceHockeyMarketPlayersParser

--- src/parsers/players/icehockey/IceHockeyPlayersParser.java
+++ src/parsers/players/icehockey/IceHockeyPlayersParser.java
@@ -23,6 +23,37 @@ public abstract class IceHockeyPlayersParser
 	@Override
 	protected IceHockeyAttributes createAttributes(Matcher matcher, boolean includeQuality)
 	{
-		return null;
+		IceHockeyAttributes attributes = new IceHockeyAttributes();
+
+		attributes.setGoa(Integer.parseInt(matcher.group("goa")));
+		attributes.setDef(Integer.parseInt(matcher.group("def")));
+		attributes.setOff(Integer.parseInt(matcher.group("off")));
+		attributes.setSho(Integer.parseInt(matcher.group("sho")));
+		attributes.setPas(Integer.parseInt(matcher.group("pas")));
+		attributes.setTec(Integer.parseInt(matcher.group("tec")));
+		attributes.setAgr(Integer.parseInt(matcher.group("agr")));
+
+		if (includeQuality)
+		{
+			attributes.setQGoa(Integer.parseInt(matcher.group("qgoa")));
+			attributes.setQDef(Integer.parseInt(matcher.group("qdef")));
+			attributes.setQOff(Integer.parseInt(matcher.group("qoff")));
+			attributes.setQSho(Integer.parseInt(matcher.group("qsho")));
+			attributes.setQPas(Integer.parseInt(matcher.group("qpas")));
+			attributes.setQTec(Integer.parseInt(matcher.group("qtec")));
+			attributes.setQAgr(Integer.parseInt(matcher.group("qagr")));
+		}
+
+		return attributes;
+	}
+
+	protected static String attributes()
+	{
+		return attributes("goa", "def", "off", "sho", "pas", "tec", "agr");
+	}
+
+	protected static String attributesWithQualities()
+	{
+		return attributesWithQualities("goa", "def", "off", "sho", "pas", "tec", "agr");
 	}
 }
