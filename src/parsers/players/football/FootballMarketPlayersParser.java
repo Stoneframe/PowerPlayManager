8b865b51494f5242e1cdafd296b5537979ca286d
Paul Steneram Bibby <paul.steneram.bibby@gmail.com>
2018-07-02 23:04:21 +0200

    Use RegexPlayersParser in football

--- src/parsers/players/football/FootballMarketPlayersParser.java
+++ src/parsers/players/football/FootballMarketPlayersParser.java
@@ -3,17 +3,44 @@ package parsers.players.football;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
-
-import model.Player;
-import model.football.FootballAttributes;
-import model.football.FootballPlayer;
-import parsers.ParseException;
-import parsers.SideParser;
-import parsers.players.PlayersParser;
+import java.util.regex.Pattern;
 
 public class FootballMarketPlayersParser
-	extends PlayersParser<FootballAttributes>
+	extends FootballPlayersParser
 {
+	private static final Pattern REGEX_PATTERN = createPattern(
+		name(),
+		ignore(), // Deadline
+		ignore(), // Cost
+		age(),
+		ignore(), // Average quality
+		cl(),
+		attributesWithQualities(),
+		experience(),
+		ignore(), // Total rating
+		side());
+
+	private static final boolean INCLUDE_CL = true;
+	private static final boolean INCLUDE_SIDE = true;
+	private static final boolean INCLUDE_QUALITIES = true;
+	private static final boolean INCLUDE_EXPERIENCE = true;
+	private static final boolean INCLUDE_CHEMISTRY = false;
+	private static final boolean INCLUDE_ENERGY = false;
+	private static final boolean INCLUDE_TRAINING = false;
+
+	public FootballMarketPlayersParser()
+	{
+		super(
+			REGEX_PATTERN,
+			INCLUDE_CL,
+			INCLUDE_SIDE,
+			INCLUDE_QUALITIES,
+			INCLUDE_EXPERIENCE,
+			INCLUDE_CHEMISTRY,
+			INCLUDE_ENERGY,
+			INCLUDE_TRAINING);
+	}
+
 	@Override
 	public String getName()
 	{
@@ -21,107 +48,17 @@ public class FootballMarketPlayersParser
 	}
 
 	@Override
-	public List<Player<FootballAttributes>> parsePlayers(String textToParse) throws ParseException
+	protected List<String> toSinglePlayerPerLine(String textToParse)
 	{
-		try
+		List<String> lines = new LinkedList<>();
+
+		String[] split = textToParse.split("\n");
+
+		for (int i = 0; i < split.length; i += 4)
 		{
-			List<Player<FootballAttributes>> players = new LinkedList<Player<FootballAttributes>>();
-
-			String[] lines = textToParse.split("\n");
-
-			for (int i = 0; i < lines.length; i += 4)
-			{
-				FootballPlayer player = new FootballPlayer(
-						parseName(lines[i]),
-						parseAge(lines[i + 3].trim().split("\t")[0].trim()),
-						parseCL(lines[i + 3].trim().split("\t")[2]),
-						SideParser.parseSide(lines[i + 3].trim().split("\t")[14]),
-						parseAttributes(Arrays.copyOfRange(lines[i + 3].trim().split("\t"), 3, 12)),
-						0,
-						0,
-						0,
-						0);
-
-				players.add(player);
-			}
-
-			return players;
+			lines.add(String.join("\t", Arrays.copyOfRange(split, i, i + 4)));
 		}
-		catch (Exception e)
-		{
-			throw new ParseException(e);
-		}
-	}
 
-	private static String parseName(String text)
-	{
-		String[] split = text.trim().split(" ");
-
-		return String.format("%s %s", split[1], split[2]);
-	}
-
-	private static int parseAge(String text)
-	{
-		return Integer.parseInt(text);
-	}
-
-	private static int parseCL(String text)
-	{
-		return Integer.parseInt(text.split("/")[0]);
-	}
-
-	private static FootballAttributes parseAttributes(String[] texts)
-	{
-		FootballAttributes attributes = new FootballAttributes();
-
-		int[] goa = parseAttribute(texts[0].trim());
-		attributes.setGoa(goa[0]);
-		attributes.setQGoa(goa[1]);
-
-		int[] def = parseAttribute(texts[1].trim());
-		attributes.setDef(def[0]);
-		attributes.setQDef(def[1]);
-
-		int[] mid = parseAttribute(texts[2].trim());
-		attributes.setMid(mid[0]);
-		attributes.setQMid(mid[1]);
-
-		int[] off = parseAttribute(texts[3].trim());
-		attributes.setOff(off[0]);
-		attributes.setQOff(off[1]);
-
-		int[] sho = parseAttribute(texts[4].trim());
-		attributes.setSho(sho[0]);
-		attributes.setQSho(sho[1]);
-
-		int[] pas = parseAttribute(texts[5].trim());
-		attributes.setPas(pas[0]);
-		attributes.setQPas(pas[1]);
-
-		int[] tec = parseAttribute(texts[6].trim());
-		attributes.setTec(tec[0]);
-		attributes.setQTec(tec[1]);
-
-		int[] spe = parseAttribute(texts[7].trim());
-		attributes.setSpe(spe[0]);
-		attributes.setQSpe(spe[1]);
-
-		int[] hea = parseAttribute(texts[8].trim());
-		attributes.setHea(hea[0]);
-		attributes.setQHea(hea[1]);
-
-		return attributes;
-	}
-
-	private static int[] parseAttribute(String text)
-	{
-		int rating = Integer.parseInt(text.substring(0, text.length() - 2));
-		int quality = Integer.parseInt(text.substring(text.length() - 2, text.length()));
-
-		return new int[]
-		{
-				rating,
-				quality
-		};
+		return lines;
 	}
 }
--- src/parsers/players/football/FootballPractiseProPlayersParser.java
+++ src/parsers/players/football/FootballPractiseProPlayersParser.java
@@ -3,23 +3,57 @@ package parsers.players.football;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
-
-import model.Player;
-import model.Side;
-import model.football.FootballAttributes;
-import model.football.FootballPlayer;
-import parsers.ParseException;
-import parsers.players.PlayersParser;
+import java.util.regex.Pattern;
 
 public class FootballPractiseProPlayersParser
-	extends PlayersParser<FootballAttributes>
+	extends FootballPlayersParser
 {
-	private static final int FIELDS_PER_PLAYER = 25;
-	private static final int AGE_FIELD = 2;
-	private static final int CL_FIELD = 4;
-	private static final int ATTRIBUTE_FIELDS_PER_PLAYER = 18;
-	private static final int ATTRIBUTES_START_FIELD = 5;
-	private static final int LAST_TRAINING_VALUE = 23;
+	private static final Pattern REGEX_PATTERN = createPattern(
+		name(),
+		ignore(), // Position
+		age(),
+		ignore(), // Scouting status
+		cl(),
+		attributeWithQuality("goa"),
+		ignore(), // Goa training
+		attributeWithQuality("def"),
+		ignore(), // Def training
+		attributeWithQuality("mid"),
+		ignore(), // Mid training
+		attributeWithQuality("off"),
+		ignore(), // Off training
+		attributeWithQuality("sho"),
+		ignore(), // Sho training
+		attributeWithQuality("pas"),
+		ignore(), // Pas training
+		attributeWithQuality("tec"),
+		ignore(), // Tec training
+		attributeWithQuality("spe"),
+		ignore(), // Spe training
+		attributeWithQuality("hea"),
+		ignore(), // Hea training
+		training());
+
+	private static final boolean INCLUDE_CL = true;
+	private static final boolean INCLUDE_SIDE = false;
+	private static final boolean INCLUDE_QUALITIES = true;
+	private static final boolean INCLUDE_EXPERIENCE = false;
+	private static final boolean INCLUDE_CHEMISTRY = false;
+	private static final boolean INCLUDE_ENERGY = false;
+	private static final boolean INCLUDE_TRAINING = true;
+
+	public FootballPractiseProPlayersParser()
+	{
+		super(
+			REGEX_PATTERN,
+			INCLUDE_CL,
+			INCLUDE_SIDE,
+			INCLUDE_QUALITIES,
+			INCLUDE_EXPERIENCE,
+			INCLUDE_CHEMISTRY,
+			INCLUDE_ENERGY,
+			INCLUDE_TRAINING);
+	}
 
 	@Override
 	public String getName()
@@ -28,116 +62,17 @@ public class FootballPractiseProPlayersParser
 	}
 
 	@Override
-	public List<Player<FootballAttributes>> parsePlayers(String textToParse) throws ParseException
+	protected List<String> toSinglePlayerPerLine(String textToParse)
 	{
-		try
+		List<String> lines = new LinkedList<>();
+
+		String[] split = textToParse.split("\n");
+
+		for (int i = 0; i < split.length; i += 19)
 		{
-			String[] lines = textToParse.replace("\n", "\t").split("\t");
-
-			List<Player<FootballAttributes>> players = new LinkedList<>();
-
-			for (int i = 0; i < lines.length; i += FIELDS_PER_PLAYER)
-			{
-				FootballPlayer player = new FootballPlayer(
-						parseName(lines[i]),
-						parseAge(lines[i + AGE_FIELD]),
-						parseCL(lines[i + CL_FIELD]),
-						Side.UNKNOWN,
-						parseAttributes(
-							Arrays.copyOfRange(
-								lines,
-								i + ATTRIBUTES_START_FIELD,
-								i + ATTRIBUTES_START_FIELD + ATTRIBUTE_FIELDS_PER_PLAYER)),
-						0,
-						0,
-						0,
-						parseTraining(lines[i + LAST_TRAINING_VALUE]));
-
-				players.add(player);
-			}
-
-			return players;
+			lines.add(String.join("\t", Arrays.copyOfRange(split, i, i + 19)));
 		}
-		catch (Exception e)
-		{
-			throw new ParseException(e);
-		}
-	}
 
-	private static String parseName(String text)
-	{
-		String[] split = text.split(" ");
-
-		return String.format("%s %s", split[1], split[2]);
-	}
-
-	private static int parseAge(String text)
-	{
-		return Integer.parseInt(text);
-	}
-
-	private static int parseCL(String text)
-	{
-		return Integer.parseInt(text.split("/")[0]);
-	}
-
-	private static FootballAttributes parseAttributes(String[] texts)
-	{
-		FootballAttributes attributes = new FootballAttributes();
-
-		int[] goa = parseAttribute(texts[0]);
-		attributes.setGoa(goa[0]);
-		attributes.setQGoa(goa[1]);
-
-		int[] def = parseAttribute(texts[2]);
-		attributes.setDef(def[0]);
-		attributes.setQDef(def[1]);
-
-		int[] mid = parseAttribute(texts[4]);
-		attributes.setMid(mid[0]);
-		attributes.setQMid(mid[1]);
-
-		int[] off = parseAttribute(texts[6]);
-		attributes.setOff(off[0]);
-		attributes.setQOff(off[1]);
-
-		int[] sho = parseAttribute(texts[8]);
-		attributes.setSho(sho[0]);
-		attributes.setQSho(sho[1]);
-
-		int[] pas = parseAttribute(texts[10]);
-		attributes.setPas(pas[0]);
-		attributes.setQPas(pas[1]);
-
-		int[] tec = parseAttribute(texts[12]);
-		attributes.setTec(tec[0]);
-		attributes.setQTec(tec[1]);
-
-		int[] spe = parseAttribute(texts[14]);
-		attributes.setSpe(spe[0]);
-		attributes.setQSpe(spe[1]);
-
-		int[] hea = parseAttribute(texts[16]);
-		attributes.setHea(hea[0]);
-		attributes.setQHea(hea[1]);
-
-		return attributes;
-	}
-
-	private static int[] parseAttribute(String text)
-	{
-		int rating = Integer.parseInt(text.substring(0, text.length() - 2));
-		int quality = Integer.parseInt(text.substring(text.length() - 2, text.length()));
-
-		return new int[]
-		{
-				rating,
-				quality
-		};
-	}
-
-	private static double parseTraining(String text)
-	{
-		return Double.parseDouble(text);
+		return lines;
 	}
 }
